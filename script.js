3eb75d0eb7af80c1355f13725c9b7bc89b74a1fe
Gabriel Neto <gabrielneto.prod@gmail.com>
2026-08-28 02:20:04 -0300

    script.js - test

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-
-  const img = document.querySelector("#profile img")
-  if (html.classList.contains("light")) {
-    
-    img.setAttribute("src", "assets/IMG_20251022_090835_019.jpg")} 
-  else {
-    img.setAttribute("src", "assets/IMG_20251022_090835_019.jpg")}
- 
-  // if (html.classList.contains("light")) {
-  //   html.classList.remove("light")
-  // } else {
-  //   html.classList.add("light")
-  // }
-}
