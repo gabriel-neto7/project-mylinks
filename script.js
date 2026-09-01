a07b0db6f848a438fc03aaead661aa91b1991991
Gabriel Neto <gabrielneto.prod@gmail.com>
2026-09-01 09:44:22 -0300

    script.js - enable

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-/*
 const toggleButton = document.getElementById('switch');
 const body = document.body;
 
-// 1. Checa se o tema salvo no navegador é o 'light'
 const savedTheme = localStorage.getItem('theme');
 
 if (savedTheme === 'light') {
-  // 2. Se for, adiciona a classe 'light' (repare que aqui não vai o ponto)
+
   body.classList.add('light');
 }
 
 toggleButton.addEventListener('click', () => {
-  // 3. Liga/Desliga a classe 'light' a cada clique
+
   body.classList.toggle('light');
-  
-  // 4. Salva a escolha atual no navegador
+
   if (body.classList.contains('light')) {
     localStorage.setItem('theme', 'light');
   } else {
     localStorage.setItem('theme', 'dark');
   }
 });
-*/
