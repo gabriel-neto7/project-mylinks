23138a9f10d8e1510e20c8b3d754dc6c39e28403
gabriel-neto7 <gabrielneto.prod@gmail.com>
2026-09-01 21:55:09 -0300

    fix - function toggleMode

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-const toggleButton = document.getElementById('switch');
-const body = document.body;
-
-const savedTheme = localStorage.getItem('theme');
-
-if (savedTheme === 'light') {
-
-  body.classList.add('light');
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
 }
-
-toggleButton.addEventListener('click', () => {
-
-  body.classList.toggle('light');
-
-  if (body.classList.contains('light')) {
-    localStorage.setItem('theme', 'light');
-  } else {
-    localStorage.setItem('theme', 'dark');
-  }
-});
